a48bbb5cdf3f43b17e876d837c3d767d6593b21a
chenhan <932322877@qq.com>
2018-12-20 16:30:33 +0800

    webgl-demo02

--- demo02/index.js
+++ demo02/index.js
@@ -1,7 +1,8 @@
 // 顶点着色器
 var VSHADER_SOURCE = 
+    'attribute vec4 a_Position;\n' +
     'void main() { \n' +
-    'gl_Position` = vec4(0.0, 0.0 , 0.0, 1.0); \n' +
+    'gl_Position = a_Position; \n' +
     'gl_PointSize = 10.0;\n' + 
     '}\n';
 
@@ -24,10 +25,19 @@ function main() {
         console.log('Failed initialize shaders');
     }
 
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+    if(a_Position < 0) {
+        console.log("Fauled to get the a_Position");
+    }
+
+    // 将顶点位置传输给 attribute 变量
+    gl.vertexAttrib3f(a_Position, 0.0,0.0,0.0);
+
     // 设置 canvas 背景色
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-    // 清空 canva
+    // 清空 canvas
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.drawArrays(gl.POINTS, 0 , 1);
